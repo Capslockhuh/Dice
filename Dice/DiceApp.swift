0c808e8416edbd1955b9958d0c7dfa2accba7e9b
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-08-05 08:38:44 +0200

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Jan Andrzejewski on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
